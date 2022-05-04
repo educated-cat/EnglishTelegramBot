329f346cc53e6ded6b6f74d10f9ad6e333b5e1ad
Ilya Malyshev <ilya_malyshev00@icloud.com>
2022-05-04 18:49:28 +0300

    Initial commit

---
+++ src/main/java/com/educatedcat/englishtelegrambot/EnglishTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.educatedcat.englishtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educatedcat/englishtelegrambot/EnglishTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educatedcat.englishtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
