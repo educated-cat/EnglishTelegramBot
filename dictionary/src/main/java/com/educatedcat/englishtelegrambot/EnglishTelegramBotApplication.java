636aa3d86fdaf77bef46eaac2da97bed1d4202ea
Ilya Malyshev <ilya_malyshev00@icloud.com>
2022-05-16 21:11:27 +0300

    ETB-6 Move "Dictionary" logic to `dictionary` module

--- dictionary/src/main/java/com/educatedcat/englishtelegrambot/EnglishTelegramBotApplication.java
+++ dictionary/src/main/java/com/educatedcat/englishtelegrambot/EnglishTelegramBotApplication.java
@@ -1,7 +1,7 @@
 package com.educatedcat.englishtelegrambot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class EnglishTelegramBotApplication {
