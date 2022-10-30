c9772e6af8c57698327d30a1c8c56056a860854e
Ilya Malyshev <ilya_malyshev00@icloud.com>
2022-10-30 10:12:21 +0300

    ENTB-7 Add deserializers to ConsumerFactory for bot tests

--- bot/src/test/java/com/educatedcat/englishtelegrambot/bot/TestKafkaConfig.java
+++ bot/src/test/java/com/educatedcat/englishtelegrambot/bot/TestKafkaConfig.java
@@ -2,8 +2,10 @@ package com.educatedcat.englishtelegrambot.bot;
 
 import com.educatedcat.englishtelegrambot.bot.user.productivity.*;
 import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.*;
 import org.springframework.context.annotation.*;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.*;
 
 @Configuration
 public class TestKafkaConfig {
@@ -11,6 +13,7 @@ public class TestKafkaConfig {
 	@Primary
 	public KafkaConsumer<Long, UserProductivityDto> userProductivityDtoKafkaConsumer(
 			ConsumerFactory<?, ?> consumerFactory) {
-		return new KafkaConsumer<>(consumerFactory.getConfigurationProperties());
+		return new KafkaConsumer<>(consumerFactory.getConfigurationProperties(), new LongDeserializer(),
+		                           new JsonDeserializer<>(UserProductivityDto.class, false));
 	}
 }
