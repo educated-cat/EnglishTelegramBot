b0a11a00c8458632fe8c18ebfb19f4031f076559
Ilya Malyshev <ilya_malyshev00@icloud.com>
2022-11-02 19:44:18 +0300

    ENTB-33 Initialize bot-sender module

---
+++ bot-sender/src/main/java/com/educatedcat/englishtelegrambot/botreceiver/BotSenderApplication.java
@@ -0,0 +1,11 @@
+package com.educatedcat.englishtelegrambot.botreceiver;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class BotSenderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BotSenderApplication.class, args);
+	}
+}
---
+++ bot-sender/src/test/java/com/educatedcat/englishtelegrambot/bot/TestKafkaConfig.java
@@ -0,0 +1,26 @@
+package com.educatedcat.englishtelegrambot.bot;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.*;
+import org.springframework.context.annotation.*;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.*;
+
+@Configuration
+public class TestKafkaConfig {
+	@Bean(destroyMethod = "close")
+	@Primary
+	public KafkaConsumer<Long, UserProductivityDto> userProductivityDtoKafkaConsumer(
+			ConsumerFactory<?, ?> consumerFactory) {
+		return new KafkaConsumer<>(consumerFactory.getConfigurationProperties(), new LongDeserializer(),
+		                           new JsonDeserializer<>(UserProductivityDto.class, false));
+	}
+	
+	@Bean(destroyMethod = "close")
+	@Primary
+	public KafkaConsumer<Long, UserDto> userDtoKafkaConsumer(
+			ConsumerFactory<?, ?> consumerFactory) {
+		return new KafkaConsumer<>(consumerFactory.getConfigurationProperties(), new LongDeserializer(),
+		                           new JsonDeserializer<>(UserDto.class, false));
+	}
+}
