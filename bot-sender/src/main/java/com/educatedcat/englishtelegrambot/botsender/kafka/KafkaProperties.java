64c021576a70ae09bc16058061ddcedb0a885ddd
Ilya Malyshev <ilya_malyshev00@icloud.com>
2022-11-04 21:32:48 +0300

    ENTB-33 Add BotReceiverListenerTest

---
+++ bot-sender/src/main/java/com/educatedcat/englishtelegrambot/botsender/kafka/KafkaProperties.java
@@ -0,0 +1,43 @@
+package com.educatedcat.englishtelegrambot.botsender.kafka;
+
+import lombok.*;
+import org.springframework.boot.context.properties.*;
+import org.springframework.stereotype.*;
+import org.springframework.validation.annotation.*;
+
+import javax.validation.constraints.*;
+
+@Data
+@Validated
+@Component
+@ConfigurationProperties("kafka")
+public class KafkaProperties {
+	@NotNull
+	private MessageSender messageSender;
+	
+	/**
+	 * Bot message sender properties
+	 */
+	@Data
+	@Validated
+	public static final class MessageSender {
+		/**
+		 * Topic name
+		 */
+		@NotNull
+		private Topic topic;
+	}
+	
+	/**
+	 * Kafka topic properties
+	 */
+	@Data
+	@Validated
+	public static final class Topic {
+		/**
+		 * Topic name
+		 */
+		@NotBlank
+		private String name;
+	}
+}
