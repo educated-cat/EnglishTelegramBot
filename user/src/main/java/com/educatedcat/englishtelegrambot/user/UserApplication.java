f880dc0ebfacd8f94e14312ae6061e2806409593
Ilya Malyshev <ilya_malyshev00@icloud.com>
2022-11-06 15:57:58 +0300

    ENTB-37 Add Eureka to all modules

--- user/src/main/java/com/educatedcat/englishtelegrambot/user/UserApplication.java
+++ user/src/main/java/com/educatedcat/englishtelegrambot/user/UserApplication.java
@@ -2,7 +2,9 @@ package com.educatedcat.englishtelegrambot.user;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.netflix.eureka.*;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserApplication {
 	
