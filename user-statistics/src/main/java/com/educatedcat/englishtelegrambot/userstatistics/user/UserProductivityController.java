d9c41a63c752289d6c624b5cf3e9c9654c3d0821
Ilya Malyshev <ilya_malyshev00@icloud.com>
2023-02-26 10:20:03 +0300

    ENTB-50 Add @ContinueSpan to UserProductivityController.getUserProductivity

--- user-statistics/src/main/java/com/educatedcat/englishtelegrambot/userstatistics/user/UserProductivityController.java
+++ user-statistics/src/main/java/com/educatedcat/englishtelegrambot/userstatistics/user/UserProductivityController.java
@@ -1,6 +1,7 @@
 package com.educatedcat.englishtelegrambot.userstatistics.user;
 
 import lombok.*;
+import org.springframework.cloud.sleuth.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserProductivityController {
 	private final UserProductivityFacade userProductivityFacade;
 	
+	@ContinueSpan(log = "Get statistics by user ID")
 	@GetMapping("/{userId}")
 	public UserProductivityDto getUserProductivity(@PathVariable long userId) {
 		return userProductivityFacade.prepareUserProductivity(userId);
